7d34839dacda539395f04f2c9f804e088bfc6bb6
Basitan Schülke <bas.schuelke@googlemail.com>
2023-02-25 12:57:17 +0100

    Added register link

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -8,8 +8,11 @@ import BaseLink from '@/components/BaseLink.vue';
         <h1 style="margin-top: 0;margin-bottom: 15px;">About</h1>
         <div>This tool provides a front-end for generating free Stable Diffusion images without needing any special hard- or software!</div><br>
         <div>If you are looking do a place to chill out, share your latest art work, discuss with other likeminded individuals, herer you go join our <BaseLink href="https://discord.gg/CuEJzkgr2C">Discord</BaseLink> (we even got all kinds of <strong>cats</strong>!).</div><br>
-        <div>Based on the original development by <BaseLink href="https://aqualxx.github.io/stable-ui/">aqualxx#5004</BaseLink></div>
-        <div>Powerd by the <BaseLink href="https://stablehorde.net/">horde network</BaseLink></div>
+        <hr>
+        <div>If you are in need of an API-Key you can get one over at <BaseLink href="https://stablehorde.net/register">https://stablehorde.net/register</BaseLink></div>
+        <hr>
+        <div><br/>Based on the original development by <BaseLink href="https://aqualxx.github.io/stable-ui/">aqualxx#5004</BaseLink></div>
+        <div><br/>Powerd by the <BaseLink href="https://stablehorde.net/">horde network</BaseLink></div>
     </div>
   </div>
 </template>
