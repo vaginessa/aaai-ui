dcc1e42ebaeabc7f21c3f56a57504a72219cea0d
Basitan Schülke <bas.schuelke@googlemail.com>
2023-03-04 11:43:21 +0100

    New invite link

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -7,7 +7,7 @@ import BaseLink from '@/components/BaseLink.vue';
     <div class="about-content">
         <h1 style="margin-top: 0;margin-bottom: 15px;">About</h1>
         <div>This tool provides a front-end for generating free Stable Diffusion images without needing any special hard- or software!</div><br>
-        <div>If you are looking do a place to chill out, share your latest art work, discuss with other likeminded individuals, herer you go join our <BaseLink href="https://discord.gg/CuEJzkgr2C">Discord</BaseLink> (we even got all kinds of <strong>cats</strong>!).</div><br>
+        <div>If you are looking do a place to chill out, share your latest art work, discuss with other likeminded individuals, herer you go join our <BaseLink href="https://discord.gg/ugEqPP5wMT">Discord</BaseLink> (we even got all kinds of <strong>cats</strong>!).</div><br>
         <div>If you want to support this endover you can head over to <BaseLink :router="false" href="https://ko-fi.com/artificialart">Ko-Fi</BaseLink> and show us some love.</div><br>
         <hr>
         <div>If you are in need of an API-Key you can get one over at <BaseLink :router="false" href="https://stablehorde.net/register">https://stablehorde.net/register</BaseLink></div>
