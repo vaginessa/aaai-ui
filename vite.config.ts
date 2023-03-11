a3ca66386df042f496956dd92a706f3c7e5d2194
Basitan Schülke <bas.schuelke@googlemail.com>
2023-03-12 00:11:38 +0100

    Trying to solving env error

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    define: {
+      global: "window",
+    },
     plugins: [
         vue(),
         ElementPlus(),
